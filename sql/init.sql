65093701e4f49446f6181698cbb79f7bcca615ec
ihni <acalj20@gmail.com>
2025-05-06 18:49:47 +0800

    fixed different names on compose and init sql file that caused an issue of ignoring the bootstrap file

--- sql/init.sql
+++ sql/init.sql
@@ -5,19 +5,18 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema payroll_sys
+-- Schema payroll_db
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema payroll_sys
+-- Schema payroll_db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `payroll_sys` DEFAULT CHARACTER SET utf8 ;
-USE `payroll_sys` ;
+USE `payroll_db` ;
 
 -- -----------------------------------------------------
--- Table `payroll_sys`.`roles`
+-- Table `payroll_db`.`roles`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `payroll_sys`.`roles` (
+CREATE TABLE IF NOT EXISTS `payroll_db`.`roles` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `hourly_rate` DECIMAL(10,2) NOT NULL,
@@ -27,46 +26,47 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `payroll_sys`.`employees`
+-- Table `payroll_db`.`employees`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `payroll_sys`.`employees` (
+CREATE TABLE IF NOT EXISTS `payroll_db`.`employees` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `role_id` INT NOT NULL,
-  `email` VARCHAR(45) NOT NULL UNIQUE,
+  `email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `role_id_idx` (`role_id` ASC),
-  CONSTRAINT `role_id`
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  CONSTRAINT `fk_employees_role_id`
     FOREIGN KEY (`role_id`)
-    REFERENCES `payroll_sys`.`roles` (`id`)
+    REFERENCES `payroll_db`.`roles` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `payroll_sys`.`work_logs`
+-- Table `payroll_db`.`work_logs`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `payroll_sys`.`work_logs` (
+CREATE TABLE IF NOT EXISTS `payroll_db`.`work_logs` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `date_worked` DATETIME NOT NULL,
   `hours_worked` DECIMAL(4,2) NOT NULL,
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `employee_id_idx` (`employee_id` ASC),
-  CONSTRAINT `employee_id`
+  CONSTRAINT `fk_worklogs_employee_id`
     FOREIGN KEY (`employee_id`)
-    REFERENCES `payroll_sys`.`employees` (`id`)
+    REFERENCES `payroll_db`.`employees` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `payroll_sys`.`payroll`
+-- Table `payroll_db`.`payroll`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `payroll_sys`.`payroll` (
+CREATE TABLE IF NOT EXISTS `payroll_db`.`payroll` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `pay_period_start` DATETIME NOT NULL,
   `pay_period_end` DATETIME NOT NULL,
@@ -75,18 +75,18 @@ CREATE TABLE IF NOT EXISTS `payroll_sys`.`payroll` (
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `employee_id_idx` (`employee_id` ASC),
-  CONSTRAINT `employee_id`
+  CONSTRAINT `fk_payroll_employee_id`
     FOREIGN KEY (`employee_id`)
-    REFERENCES `payroll_sys`.`employees` (`id`)
+    REFERENCES `payroll_db`.`employees` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `payroll_sys`.`organization`
+-- Table `payroll_db`.`organization`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `payroll_sys`.`organization` (
+CREATE TABLE IF NOT EXISTS `payroll_db`.`organization` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `total_salary_budget` DECIMAL(10,2) NOT NULL,
