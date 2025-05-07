f55a6a411461bec4d28fde4e7a8a6bf36f24015a
ihni <acalj20@gmail.com>
2025-05-07 22:41:42 +0800

    cleaned up employee service and added get by role and email

--- app/services/employee_service.py
+++ app/services/employee_service.py
@@ -19,6 +19,7 @@ class EmployeeService:
             VALUES
             (%(first_name)s, %(last_name)s, %(email)s, %(role_id)s)
         """
+
         try:
             cursor.execute(query, employee.to_dict())
             cnx.commit()
@@ -43,13 +44,14 @@ class EmployeeService:
             SELECT * from employees
             WHERE id = %s
         """
+
         try:
             cursor.execute(query, (emp_id,))
             row = cursor.fetchone()
-            logger.info("Fetched employee id: (%s), returning results.", emp_id)
+            logger.info("Fetched employee id (%s), returning results.", emp_id)
             return Employee(**row) if row else None
         except MySQLError as err:
-            logger.error("Failed to fetch employee id: (%s): %s", emp_id, err)
+            logger.error("Failed to fetch employee id (%s): %s", emp_id, err)
             return None
         finally:
             cursor.close()
@@ -66,17 +68,42 @@ class EmployeeService:
             SELECT * from employees
             WHERE email = %s
         """
+
         try:
             cursor.execute(query, (emp_email,))
             row = cursor.fetchone()
-            logger.info("Fetched employee email: (%s), returning results.", emp_email)
+            logger.info("Fetched employee email (%s), returning results.", emp_email)
             return Employee(**row) if row else None
         except MySQLError as err:
-            logger.error("Failed to fetch employee email: (%s): %s", emp_email, err)
+            logger.error("Failed to fetch employee email (%s): %s", emp_email, err)
             return None
         finally:
             cursor.close()
-    
+
+    @staticmethod
+    def get_by_role_id(role_id: int) -> list[Employee]:
+        cnx = db.get_connection()
+        if not cnx:
+            logger.error("No database connection available.")
+            return []
+        cursor = cnx.cursor(dictionary=True)
+        
+        query = """
+            SELECT * from employees
+            WHERE role_id = %s
+        """
+
+        try:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            logger.info("Fetched all employees with role (%s), returning results.", role_id)
+            return [Employee(**row) for row in rows] if rows else []
+        except MySQLError as err:
+            logger.error("Failed to fetch employees with role (%s): %s", role_id, err)
+            return []
+        finally:
+            cursor.close()
+
     @staticmethod
     def update(emp_id: int, update_fields: dict) -> bool:
         # nothing to update
@@ -110,7 +137,7 @@ class EmployeeService:
         try:
             cursor.execute(query, values)
             cnx.commit()
-            logger.info("Executed update on employee id(%s), returning results", emp_id)
+            logger.info("Executed update on employee id (%s), returning results", emp_id)
             return cursor.rowcount > 0
         except MySQLError as err:
             cnx.rollback()
@@ -135,10 +162,10 @@ class EmployeeService:
         try:
             cursor.execute(query, (emp_id,))
             cnx.commit()
-            logger.info("Executed deletion on employee id: (%s), returning results", emp_id)
+            logger.info("Executed deletion on employee id (%s), returning results", emp_id)
             return cursor.rowcount > 0
         except MySQLError as err:
-            logger.error("Failed to delete employee id: (%s): %s", emp_id, err)
+            logger.error("Failed to delete employee id (%s): %s", emp_id, err)
             return False
         finally:
             cursor.close()
